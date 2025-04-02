fdcaf94b07c0ea3a35addb2bbe7aacd0b77ef441
Mahdi <mahdi.zed2002@gmail.com>
2025-04-02 20:43:38 +0330

    create villa controller v2

---
+++ Controllers/V2/VillaV2Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Services.Villa;
+
+namespace OnlineShop.Controllers.V2
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaV2Controller : ControllerBase
+    {
+        private readonly IVillaService _villaService;
+        public VillaV2Controller(IVillaService villaService)
+        {
+            _villaService = villaService;
+        }
+
+        [HttpGet]
+        public IActionResult Search(int pageId = 1, string filter = "", int take = 2)
+        {
+            if (pageId < 1 || take < 1) return BadRequest();
+            var data = _villaService.SerachVilla(pageId, filter, take);
+            return Ok(data);
+        }
+    }
+}
