d0552da8d1283ecd9255a5e35ba0d379914935c0
Mahdi <mahdi.zed2002@gmail.com>
2025-04-03 15:22:45 +0330

    change action name in villav2 controller

--- Controllers/V2/VillaV2Controller.cs
+++ Controllers/V2/VillaV2Controller.cs
@@ -5,8 +5,8 @@ using OnlineShop.Services.Villa;
 
 namespace OnlineShop.Controllers.V2
 {
-    [Route("api/v{version:apiVersion}/villa")]
     [ApiVersion(2.0)]
+    [Route("api/v{version:apiVersion}/villa")]
     [ApiController]
     public class VillaV2Controller : ControllerBase
     {
@@ -16,8 +16,8 @@ namespace OnlineShop.Controllers.V2
             _villaService = villaService;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Search(int pageId = 1, string filter = "", int take = 2)
+        [HttpGet("[action]")]
+        public async Task<IActionResult> getDataAsync(int pageId = 1, string filter = "", int take = 2)
         {
             if (pageId < 1 || take < 1) return BadRequest();
             var data = await _villaService.SerachVillaAsync(pageId, filter, take);
